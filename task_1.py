095d027d751f8b2d5ada06a5ca78649f052ca267
Natalya Ch <n_babaeva@mail.ru>
2023-09-06 22:18:49 +0300

    Выполнено тринадцатое ДЗ

---
+++ task_1.py
@@ -0,0 +1,86 @@
+'''
+Создайте класс Матрица. Добавьте методы для: - вывода на печать,
+сравнения,
+сложения,
+*умножения матриц
+'''
+
+
+
+class Matrix:
+
+    def __init__(self, matr):
+        self._matr = matr
+
+    def get_matrix(self):
+        return self._matr
+
+    def __add__(self, other):
+        if len(self._matr) != len(other._matr) or len(self._matr[0]) != len(other._matr[0]):
+            raise Exception('Error', '"матрицы разных размедств"')
+        else:
+            return Matrix([[self._matr[i][j] + other._matr[i][j] for j in range(len(self._matr[0]))] for i in range(len(self._matr))])
+
+    def __mul__(self, other):
+        if len(self._matr[0]) != len(other._matr):
+            raise Exception('Error', '"невозможно перемножить матдств"')
+        else:
+            mult_matr = [[sum(i * j for i, j in zip(i_row, j_col)) for j_col in zip(*other._matr)] for i_row in self._matr]
+            return Matrix(mult_matr)
+
+    def __eq__(self, other):
+        if len(self._matr) != len(other._matr) or len(self._matr[0]) != len(other._matr[0]):
+            raise Exception('Error', '"матрицы разных размедств"')
+        else:
+            for i in range(len(self._matr)):
+                for j in range(len(self._matr[0])):
+                    if self._matr[i][j] != other._matr[i][j]:
+                        return False
+            return True
+
+    def __str__(self):
+        s = ''
+        for i in range(len(self._matr)):
+            s += str(self._matr[i]) + '\n'
+        return s
+
+m_1 = [[5, 2, 1],
+          [7, 4,  2],
+          [5, 7, 9],
+          [5, 5, 5]]
+
+m_2 = [[1, 2, 1],
+          [8, 5,  8],
+          [2, 2,  2],
+          [8, 8, 3]]
+
+m_3 = [[9, 8, 7, 6],
+          [5, 4, 3, 2],
+          [1, 0, -1, -2]]
+
+
+matr_1 = Matrix(m_1)
+matr_2 = Matrix(m_2)
+matr_3 = Matrix(m_3)
+
+
+print ("Cложение матриц:")
+try:
+    matr_sum = matr_1 + matr_2
+    print(matr_sum)
+except Exception as e:
+    print(f'Ошибка сложения матриц, {e}')
+
+print ("Умножение матриц:")
+try:
+    print(matr_1 * matr_3)
+except Exception as e:
+    print(f'Ошибка сложения матрицнег, {e}')
+
+
+print ("Cравнение матриц:")
+try:
+    print(matr_1 == matr_1)
+    print(matr_3 == matr_2)
+except Exception as e:
+    print(f'Ошибка сложения матрицнег, {e}')
---
+++ task_2.py
@@ -0,0 +1,77 @@
+"""Напишите программу банкомат.
+✔ Начальная сумма равна нулю
+✔ Допустимые действия: пополнить, снять, выйти
+✔ Сумма пополнения и снятия кратны 50 у.е.
+✔ Процент за снятие — 1.5% от суммы снятия, но не менее 30 и не более 600 у.е.
+✔ После каждой третей операции пополнения или снятия начисляются проценты - 3%
+✔ Нельзя снять больше, чем на счёте
+✔ При превышении суммы в 5 млн, вычитать налог на богатство 10% перед каждой
+операцией, даже ошибочной
+✔ Любое действие выводит сумму денег"""
+
+summ = 0
+count_add = 0
+count_out = 0
+
+
+def withdraw(summ, count_out):
+    summ_out = int(input("Сумма: "))
+    comission = summ_out * 0.015
+    if comission < 30:
+        comission = 30
+    elif comission > 600:
+        comission = 600
+
+    if summ_out + comission > summ:
+        raise Exception('Error', "Недостаточно средств")
+
+    else:
+        if summ_out % 50 == 0:
+            summ -= summ_out + comission
+
+            count_out += 1
+            if count_out % 3 == 0:
+                summ *= 1.03
+        else:
+            raise Exception('Error', "Введена некорректная сумма")
+    return summ
+
+
+def add_money(summ, count_add):
+    summ_add = int(input("Сумма: "))
+    if summ_add % 50 == 0:
+        summ += summ_add
+        count_add += 1
+        if count_add % 3 == 0:
+            summ *= 1.03
+    else:
+        print("Введена некорректная сумма (не кратна 50)")
+    return summ
+
+
+def tax(summ):
+    if summ > 5_000_000:
+        print("С вас сняли налог на богатство", summ * 0.1)
+        summ -= summ * 0.1
+    return summ
+
+
+while True:
+    summ = tax(summ)
+
+    action = input("Действие: ")
+
+    if action == "q":
+        print("Выходим из банкомата")
+        print("Сумма: ", summ)
+        break
+    elif action == "a":
+        summ = add_money(summ, count_add)
+
+    elif action == "o":
+        try:
+            summ = withdraw(summ, count_out)
+        except Exception as e:
+            print(f'Ошибка снятия денег, {e}')
+
+    print(f"Сумма: {summ}")
